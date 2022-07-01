1a5b5c87667ff596bda1a73619152776598d4b65
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-01 17:45:25 -0500

    database layout

--- database-seeder/database/db/migration.sql
+++ database-seeder/database/db/migration.sql
@@ -1,26 +1,21 @@
--- DROP DATABASE IF EXISTS amazon;
+DROP DATABASE IF EXISTS amazon;
 
--- CREATE DATABASE amazon;
--- \c amazon
+CREATE DATABASE amazon;
+\c amazon
 
--- DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS product;
 
 CREATE TABLE product(
     product_id SERIAL PRIMARY KEY,
-    asin_id VARCHAR,
     product_name VARCHAR,
-    picture TEXT ARRAY,
+    picture TEXT[],
     rating NUMERIC,
-    product_dimensions VARCHAR,
-    item_weight VARCHAR,
-    manufacturer_name VARCHAR,
-    country TEXT,
-    model_number VARCHAR,
-    batteries VARCHAR,
-    date_available VARCHAR,
-    box_info TEXT ARRAY
-    review_id INT REFERENCES review(review_id)
-    question id INT REFERENCES qanda(question_id)
+    reviews INTEGER,
+    category TEXT,
+    price NUMERIC,
+    options TEXT[],
+    about TEXT[],
+    asin_id VARCHAR
 );
 
 CREATE TABLE qanda(
@@ -28,13 +23,34 @@ CREATE TABLE qanda(
     question_title TEXT,
     question_date VARCHAR,
     answer_text TEXT,
-    asin_id VARCHAR REFERENCES product(asin_id)
+    question_user VARCHAR,
+    asin_id VARCHAR 
 );
 
 CREATE TABLE review(
-    review_id INT PRIMARY KEY,
+    review_id SERIAL PRIMARY KEY,
     rating NUMERIC,
-    asin_id VARCHAR REFERENCES product(asin_id)
     review_title TEXT,
-    review_body TEXT
+    review_body TEXT,
+    asin_id VARCHAR
 );
+
+CREATE TABLE product_specs (
+    product_id SERIAL PRIMARY KEY NOT NULL,
+    product_dimensions VARCHAR,
+    item_weight VARCHAR,
+    manufacturer_name VARCHAR,
+    country VARCHAR,
+    model_number VARCHAR,
+    batteries VARCHAR,
+    date_available VARCHAR,
+    box_info text[],
+    asin_id VARCHAR
+    );
+
+CREATE TABLE product_categories(
+    category_id SERIAL PRIMARY KEY,
+    category TEXT,
+    asin_id INTEGER[]
+)
+    

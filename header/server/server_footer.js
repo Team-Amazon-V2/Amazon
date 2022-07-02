67a7892f1a4b86cbc975dae0d09992afdfe6688d
Michael Howard <mike.gene.howard@gmail.com>
2022-07-01 14:54:23 -1000

    removed unused footer server.js

--- header/server/server_footer.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(json());
-
-const PORT = 3013;
-
-app.get("/amazon/footer", async (req, res) => {});

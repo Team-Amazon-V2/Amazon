0dc315d0b67d04e2f5fb1aac40a4d14aef2963ec
Yanchao Jin <yanchaojin@YanchaodeiMac.local>
2022-07-03 12:28:21 -0500

    update

--- overview/db/conn.js
+++ overview/db/conn.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user:"postgres",
-    password:"chao0629",
-    database:"productoverview",
-    host:"localhost",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+       rejectUnauthorized: false,
+    },
   
 });
 

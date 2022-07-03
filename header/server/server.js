9abdb19d9453fbd3610a2267c134d9c95a863bee
Michael Howard <mike.gene.howard@gmail.com>
2022-07-03 09:17:57 -1000

    Modified Header server.js to fetch from Heroku db

--- header/server/server.js
+++ header/server/server.js
@@ -6,16 +6,17 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3012" || "http://localhost:3000",
+    origin: "http://localhost:3012",
     methods: ["GET"],
   })
 );
 
 const poolConfig = {
-  host: "localhost",
-  user: "atlantis",
-  database: "amazon_clone",
-  port: 5432,
+  connectionString:
+    "postgres://fnhxvljpuvguhu:1d6974f136a4cee343e385afc17e6a7a9c4fcff038452b069f6512af88b5454f@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d30mp7kuvm55ck",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool(poolConfig);
@@ -26,7 +27,7 @@ app.get("/amazon/:item", async (req, res) => {
   const { item } = req.params;
   try {
     const data = await pool.query(
-      `SELECT * FROM items WHERE name ~* '${item}';`
+      `SELECT * FROM product WHERE product_name ~* '${item}';`
     );
     console.log(data.rows);
     res.json(data.rows);

6df42949f3ec1ea061b9de1a5f627ebd0f0ee6db
magdielrosario97 <magdielrosario97@gmail.com>
2022-09-09 19:40:23 -0500

    saving changes

--- database-seeder/database/db/ppl-also-bought-seed.sql
+++ database-seeder/database/db/ppl-also-bought-seed.sql
@@ -1,5 +1,17 @@
 INSERT INTO related_products (asin_id, picture, product_name, rating, reviews, category, price)
     VALUES
-    ('B09V3HN1KC', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-select-wifi-silver-202104_FMT_WHH?wid=1945&hei=2000&fmt=jpeg&qlt=95&.v=1617126626000',
-    '2021 Apple 11-inch iPad Pro (Wi-Fi, 128GB) - Silver',
-    4.9, 5180, 'Electronics', 749.00);
+    ('B00TREI0F2', 'https://m.media-amazon.com/images/I/81twYxZsx0L._AC_SX522_.jpg',
+    'Apple iPad Air 2, 64 GB, Space Gray (Renewed)',
+    4.4, 18974, 'Electronics', 160.00);
+
+INSERT INTO related_products (asin_id, picture, product_name, rating, reviews, category, price)
+    VALUES
+    ('B09N41RC6L', 'https://m.media-amazon.com/images/I/61krikJxTmL._AC_SX522_.jpg',
+    'Samsung Galaxy Tab A8 Android Tablet, 10.5” LCD Screen, 32GB Storage, Long-Lasting Battery, Kids Content, Smart Switch, Expandable Memory, Dark Gray',
+    4.6, 3499, 'Electronics', 198.26);
+
+INSERT INTO related_products (asin_id, picture, product_name, rating, reviews, category, price)
+    VALUES
+    ('B09G9C4526', 'https://m.media-amazon.com/images/I/71qV9xzdxzL._AC_SX679_.jpg',
+    '2021 Apple iPad Mini (Wi-Fi + Cellular, 64GB) - Starlight',
+    4.8, 4491, 'Electronics', 629.98);
